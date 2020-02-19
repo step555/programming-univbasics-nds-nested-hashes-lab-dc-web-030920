a55b2160e69ec7569d4ec971b666ce4ea3f950c3
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-19 23:10:07 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -2,7 +2,7 @@
 
 DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
 JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
-PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
+PAT_M =  { { name:  "Joelle van Dyne", occupation: "Radio Personality" } }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
@@ -15,7 +15,11 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  
+  array = 
+  [{ name:  "Don Gately", occupation: "Live-in Staff" }, 
+   { name:  "Joelle van Dyne", occupation: "Radio Personality"} ,{ name:  "Joelle van Dyne", occupation: "Radio Personality" }, 
+   { name:  "Kate Gompert", occupation: "None" },
+   { name:  "Bruce Green", occupation: "Fan of Mildred" }]
 end
 
 def aoh_lookup(aoh, row, key)
