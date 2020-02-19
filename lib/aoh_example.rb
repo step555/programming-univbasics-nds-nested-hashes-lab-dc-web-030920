1e67028f41cc84cfb695800c7c96544498293083
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-19 23:12:12 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,7 +15,6 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  array = { name:  "Joelle van Dyne", occupation: "Radio Personality",
 end
 
 def aoh_lookup(aoh, row, key)
